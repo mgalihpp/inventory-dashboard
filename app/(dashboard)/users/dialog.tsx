23b7c6a1dbde46ec06420c7258f807330b270013
mgalihpp <muhammadgalih451@gmail.com>
2024-02-26 13:13:29 +0700

    fix text styles

--- app/(dashboard)/users/dialog.tsx
+++ app/(dashboard)/users/dialog.tsx
@@ -317,7 +317,7 @@ export function AddUserDialog() {
           variant="ghost"
           size="sm"
           type="button"
-          className="flex items-center gap-2 justify-center text-white 
+          className="flex items-center gap-2 justify-center text-gray-100 hover:text-white 
           bg-green-500 hover:bg-green-600 focus:ring-2 focus:outline-none 
           focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
           dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
